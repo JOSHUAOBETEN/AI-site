df4fa076be50cb6835a95ff54e56e66f68abf72c
Ojowa Odunayo <hunchoweb@gmail.com>
2022-10-31 17:30:01 +0100

    Structured all the folders and files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
 const app = () => {
   return (
     <div>
-        <h1>GPT-3</h1>
+        
     </div>
   )
 }
