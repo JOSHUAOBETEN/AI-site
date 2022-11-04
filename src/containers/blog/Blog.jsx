b26fdb3f8c640bd25579cf831befe018bea9fb47
Ojowa Odunayo <hunchoweb@gmail.com>
2022-11-04 09:50:38 +0100

    Updated the css for the entire blog.jsx section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -5,8 +5,8 @@ import './blog.css';
 
 const Blog = () => {
   return (
-    <div className='gpt3__blob section__padding' id='blog'>
-        <div classname='gpt3__blob-heading'>
+    <div className='gpt3__blog section__padding' id='blog'>
+        <div className='gpt3__blog-heading'>
           <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
         </div>
         <div className='gpt3__blog-container'>
